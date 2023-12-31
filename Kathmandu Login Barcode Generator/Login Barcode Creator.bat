2f1e72c089007113444a02aac74abd5c61cef08e
Roshan Varughese <40583749+Animeboynz@users.noreply.github.com>
2024-01-01 11:12:59 +1300

    Adds Author Watermark

--- Kathmandu Login Barcode Generator/Login Barcode Creator.bat
+++ Kathmandu Login Barcode Generator/Login Barcode Creator.bat
@@ -17,6 +17,25 @@ setlocal enabledelayedexpansion
 set "search=^XA^FO0,0"
 set "replace=^XA^FO25,50"
 set "input_file=temp.txt"
+set "output_file=temp2.txt"
+rem Read the input file line by line and replace the first occurrence of the search string
+set "found=0"
+(
+    for /f "delims=" %%a in (%input_file%) do (
+        if !found!==0 (
+            set "line=%%a"
+            set "line=!line:%search%=%replace%!"
+            echo !line!
+            set "found=1"
+        ) else (
+            echo %%a
+        )
+    )
+) > %output_file%
+
+set "search=^XZ"
+set "replace=^CF0,15^FO0,170^FD----------------------------^FS^CF0,15^FO10,180^FDCut Here^FS^CF0,30^FO32,215^FDDesigned by Roshan V^FS^CF0,22^FO30,250^FDhttps://github.com/Animeboynz^FS^XZ"
+set "input_file=temp2.txt"
 set "output_file=Login_Barcode.txt"
 rem Read the input file line by line and replace the first occurrence of the search string
 set "found=0"
@@ -41,6 +60,7 @@ pause
 
 DEL image.gif
 DEL temp.txt
+DEL temp2.txt
 DEL Login_Barcode.txt
 
 exit 0
